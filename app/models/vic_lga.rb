6950ddb8f34ca478abfaf95da659f6b745d2231b
Mark Benjamin Puerto <krampuerto@gmail.com>
2024-01-25 15:29:36 +0800

    Add annotate gem to list all fields in table
    Signed-off-by: Mark Benjamin Puerto <krampuerto@gmail.com>

--- app/models/vic_lga.rb
+++ app/models/vic_lga.rb
@@ -1,2 +1,21 @@
+# == Schema Information
+#
+# Table name: vic_lgas
+#
+#  id         :bigint           not null, primary key
+#  pfi        :string
+#  lga_code   :string
+#  lga_name   :string
+#  gaz_lga    :string
+#  gazregn    :string
+#  abslgacode :string
+#  pfi_cr     :date
+#  ufi        :integer
+#  ufi_cr     :date
+#  ufi_old    :integer
+#  geom       :geometry         geometry, 0
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class VicLga < ApplicationRecord
 end
